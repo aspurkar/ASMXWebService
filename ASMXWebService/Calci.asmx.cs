919890ea1caeada43ea0b035071281b5130947ca
Aditya Purkar <aspurkar@gmail.com>
2017-05-13 11:27:32 -0700

    Final Commit

---
+++ ASMXWebService/Calci.asmx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ASMXWebService
+{
+    /// <summary>
+    /// Summary description for Calci
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Calci : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+         [WebMethod]
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+         [WebMethod]
+         public ComplexNumber AddComplex(ComplexNumber c1, ComplexNumber c2)
+         {
+             ComplexNumber c = new ComplexNumber();
+             c.I = c1.I + c2.I;
+             c.J = c1.J + c2.J;
+             return c;
+         }
+    }
+    public class ComplexNumber
+    {
+        public int I { get; set; }
+        public int J { get; set; }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApplication1.ServiceRef;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calci client = new Calci();
+            client.Url = "http://localhost:1125/Calci.asmx";
+            Console.WriteLine(client.Add(1, 5));
+            ComplexNumber c1 = new ComplexNumber { I = 4, J = 5 };
+            ComplexNumber c2 = new ComplexNumber { I = 3, J = 4 };
+            var c = client.AddComplex(c1, c2);
+            Console.WriteLine(c.I +" + "+ c.J + "j");
+            Console.Read();
+        }
+    }
+}
